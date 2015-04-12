4c8444d022bb4fa8e2b32b251798708d40b305bc
ClementBoudon <clement.boudon@gmail.com>
2015-04-12 22:39:24 +0200

    Gestion cache BDD domains.

--- web/api/v1/web/index.php
+++ web/api/v1/web/index.php
@@ -1,4 +1,6 @@
 <?php
+    date_default_timezone_set('GMT');
+
     require_once __DIR__.'/../vendor/autoload.php';
 
     use PhpWhois\PhpWhois;
@@ -190,41 +192,91 @@
     });
 
     /*
-    GET /domains/{domain}/availabe
+    GET /domains/{domain}/available
     Retourne true pour la propriété "available" si le domaine est disponible.
     Utilisation du provider phpWhois https://github.com/phpWhois/phpWhois
     */
 
-    $app->get('/api/v1/domains/{domain}/availabe',
+    $app->get('/api/v1/domains/{domain}/available',
         function (Silex\Application $app, $domain) {
 
             $domain_escaped = $app->escape(urldecode($domain));
 
-            $availabe = false;
+            $available = false;
 
-            $app['PhpWhois']->deepWhois = false;
-            $result = $app['PhpWhois']->lookup($domain_escaped,false);
-
-            if($result['regrinfo']['registered'] == 'no')
+            //On contrôle en Base si nous avons l'info depuis moins d'un jour
+            $available_cache = null;
+            $sql = "SELECT id, available, dt_caching
+            FROM Domains
+            WHERE name = ?";
+            $stmt = $app['db']->prepare($sql);
+            $stmt->bindValue(1, $domain_escaped);
+            $stmt->execute();
+            if($row = $stmt->fetch())
             {
-                $availabe = true;
+                if($row['dt_caching'] < date("Y-m-d H:i:s",(time() + (1*24*60*60))))
+                {
+                    //Le cache est toujours valide
+                    $available_cache = (bool) $row['available'];
+                }
+                else
+                {
+                    //Le cache est expiré, on le supprime
+                    $sql_del = "DELETE
+                    FROM Domains
+                    WHERE name = ?";
+                    $stmt_del = $app['db']->prepare($sql_del);
+                    $stmt_del->bindValue(1, $domain_escaped);
+                    $stmt_del->execute();
+                }
+            }
+            if($available_cache !== null)
+            {
+                //S'il y avait un cache valide
+                $available = $available_cache;
+            }
+            else
+            {
+                //S'il n'y avait pas de cache valide
+                $app['PhpWhois']->deepWhois = false;
+                $result = $app['PhpWhois']->lookup($domain_escaped,false);
+
+                if($result['regrinfo']['registered'] == 'no')
+                {
+                    $available = true;
+                }
+
+                //On enregistre l'info en cache
+                $sql_ins = "INSERT
+                INTO Domains
+                (name, available, dt_caching)
+                VALUES
+                (?,?,?)";
+                $stmt_ins = $app['db']->prepare($sql_ins);
+                $stmt_ins->bindValue(1, $domain_escaped);
+                $stmt_ins->bindValue(2, $available);
+                $stmt_ins->bindValue(3, date('Y-m-d H:i:s'));
+                $stmt_ins->execute();
+
             }
 
-            return json_encode($availabe);
+
+
+            return json_encode($available);
     });
 
     /*
-    GET /brands/{brand}/availabe?ipoffices[]=
+    GET /brands/{brand}/available?ipoffices[]=
     Retourne true pour la propriété "available" si la marque est disponible
     pour le bureau de propriété intellectuel choisi
     TODO
     */
-    $app->get('/api/v1/brands/{brand}/availabe',
+    $app->get('/api/v1/brands/{brand}/available',
         function (Silex\Application $app, $brand) {
 
             $brand_escaped = $app->escape(urldecode($brand));
 
-            $availabe = false;
+            $available = false;
 
             $array_ipoffices = array();
 
@@ -236,7 +288,7 @@
                 }
             }
 
-            return json_encode($availabe);
+            return json_encode($available);
     });
 
     /*
