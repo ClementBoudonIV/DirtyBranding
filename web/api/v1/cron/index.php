68b43e2163fad7cc705e0c58535ad493eccc30f0
ClementBoudon <clement.boudon@gmail.com>
2015-04-21 08:30:26 +0200

    Cron import base INPI Stock

--- web/api/v1/cron/index.php
+++ web/api/v1/cron/index.php
@@ -1,6 +1,12 @@
 <?php
     date_default_timezone_set('GMT');
 
+    require_once __DIR__.'/vendor/autoload.php';
+
+    use Prewk\XmlStringStreamer;
+    use Prewk\XmlStringStreamer\Parser;
+    use Prewk\XmlStringStreamer\Stream;
+
     //Old PHP Style
 
     $link = new PDO("mysql:dbname=DB_API;host=localhost", "root", "root");
@@ -8,20 +14,24 @@
     $xml_inpi_folder  = __DIR__.'/../../../../Ressources/INPI/XML/';
 
     //listing des fichiers XML dispo
-    $list_files = scandir($xml_inpi_folder);
-    $list_files = array_diff($list_files, array('.', '..'));
+    $list_files = glob($xml_inpi_folder.'*.xml');
+
+    //$pattern = "~^(".$xml_inpi_folder."FR_FRNEWST66_|".$xml_inpi_folder."ST66_)*.xml$~";
 
     //on parcours chacun des fichiers
-    foreach ($list_files as $files) {
-        echo 'Traitement : '.$xml_inpi_folder.$files.' ...';
+    foreach ($list_files as $xml_inpi_absolute_path) {
 
-        $xml = simplexml_load_file($xml_inpi_folder.$files);
+        echo 'Traitement : '.$xml_inpi_absolute_path.' ...';
 
-        //on récupère simplement le nom de a marque, et son code
-        foreach($xml->TradeMarkTransactionBody->TransactionContentDetails->TransactionData->TradeMarkDetails->TradeMark as $TradeMark){
+
+        $streamer = Prewk\XmlStringStreamer::createUniqueNodeParser($xml_inpi_absolute_path, array("uniqueNode" => "TradeMark"));
+
+
+        while ($node = $streamer->getNode()) {
+
+            $TradeMark = simplexml_load_string($node);
 
             $brand_name = $TradeMark->WordMarkSpecification->MarkVerbalElementText;
-
             $sql_ins = "INSERT
             INTO BrandsINPI
             (name, dt_caching)
@@ -37,7 +47,6 @@
             $ClassDescriptionDetails = $TradeMark->GoodsServicesDetails->GoodsServices->ClassDescriptionDetails->ClassDescription;
             $array_class = array();
             foreach ($ClassDescriptionDetails as $xml_class) {
-
                 $sql_ins = "INSERT
                 INTO BrandsClassesINPI
                 (id_brand, classe)
@@ -47,13 +56,11 @@
                 $stmt_ins->bindValue(1, $id_brand);
                 $stmt_ins->bindValue(2, $xml_class->ClassNumber);
                 $stmt_ins->execute();
-
             }
         }
 
         echo ' [OK]
 ';
-
 
     }
 
